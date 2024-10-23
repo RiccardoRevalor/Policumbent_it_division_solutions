f48741123a1af3146f96e374d6eb03fd52ff9322
RiccardoRevalor <riccardorevalor@gmail.com>
2024-10-23 10:05:05 +0200

    c-task added solution c00

--- c-task/app_gnss.c
+++ c-task/app_gnss.c
@@ -29,6 +29,16 @@ static GNSSParser_Data_t GNSSParser_Data;
 static uint32_t getValue() {
     
     /* USER CODE BEGIN 1 */     //Insert code if necessary
+
+    //max value for an 8-bit integer unsigned
+    //rand() % (max_number + 1 - minimum_number) + minimum_number
+    //rand() % (255 + 1 - 0) + 0
+
+    //rand() returns 32 bit integers. 
+    //Since the function returns a 32 bit integer, the return value is stored in a 32 bit integer variable.
+    //the last 8 bits of the 32 bit integer variable are the random number between 0 and 255.
+    return (rand() % 256); 
+    
     
     /* USER CODE END 1 */
 
@@ -47,7 +57,6 @@ void task(void *args){
     uint32_t value;
 
     /* USER CODE BEGIN 1 */     //Insert code if necessary
-    
 
     /* USER CODE END 1 */
 
@@ -55,6 +64,13 @@ void task(void *args){
     HAL_Init();
 
     /* USER CODE BEGIN 2 */     //Insert code if necessary
+
+    //GNSSParser_Data initialization
+    if GNSS_PARSER_Init(&GNSSParser_Data) != GNSS_PARSER_OK {
+        //ERROR: GNSSParser_Data initialization failed
+        printf("ERROR: GNSSParser_Data initialization failed\n");
+        return -1;
+    }
 
     /* Initialization of the Parser*/
 
@@ -65,6 +81,16 @@ void task(void *args){
         value = getValue();
         /* USER CODE BEGIN WHILE */     //Insert code if necessary
 
+        if (value & (1 << 2)) {  // Third bit -> GPGGA
+            GNSS_PARSER_ParseMsg(&GNSSParser_Data, GPGGA); //analysis of the message
+            GNSS_PARSER_Print(&GNSSParser_Data, GPGGA); //print the message
+        } else if (value & (1 << 1)) {  // Second bit -> GNS
+            GNSS_PARSER_ParseMsg(&GNSSParser_Data, GNS);
+            GNSS_PARSER_Print(&GNSSParser_Data, GNS);
+        } else if (value & (1 << 0)) {  // First bit -> GPRMC
+            GNSS_PARSER_ParseMsg(&GNSSParser_Data, GPRMC);
+            GNSS_PARSER_Print(&GNSSParser_Data, GPRMC);
+        }
 
         /* USER CODE END WHILE */
 
